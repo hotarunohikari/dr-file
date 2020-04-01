f7da6141e58c8070f34300a316194174b29167a0
wxl <1445598659@qq.com>
2020-04-01 14:48:20 +0800

    first commit

---
+++ src/file/DrFile.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * author : hotarunohikari
+ */
+
+namespace dr\file;
+
+
+class DrFile
+{
+
+    // 前缀修正
+    static function drFixPrefix($path) {
+        $prefix = substr($path, 0, 1);
+        if ($prefix == "/" || $prefix == "\\") {
+            $path = substr($path, 1);
+        }
+        return $path;
+    }
+
+    // 尾缀修正
+    static function drFixSuffix($path) {
+        $suffix = substr($path, -1, 1);
+        if (!($suffix == "/" || $suffix == "\\")) {
+            $path = $path . "/";
+        }
+        return $path;
+    }
+
+    // 根据日期创建目录,多用于文件上传存储
+    static function drMkdirByDate($dir) {
+        $dir = self::drFixPrefix($dir);
+        $dir = self::drFixSuffix($dir);
+        $dir = $dir . date("Y") . "/" . date("md") . "/";
+        if (!is_dir($dir)) {
+            try {
+                mkdir($dir, 0777, true);
+            } catch (\Exception $e) {
+                throw new Exception("目录创建失败");
+            }
+        }
+        return $dir;
+    }
+
+    // 生成一个不可能重复的名字
+    static function drMakeName($prefix = 'dr') {
+        return session_create_id($prefix);
+    }
+
+}
