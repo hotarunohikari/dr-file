3ea2fdc6a832fe21fc3d8d389864aa9d40ef1ce6
wxl <1445598659@qq.com>
2020-04-07 10:37:11 +0800

    add params

--- src/file/DrFile.php
+++ src/file/DrFile.php
@@ -10,22 +10,51 @@ namespace dr\file;
 class DrFile
 {
 
-    // 前缀修正
-    static function drFixPrefix($path) {
+    /**
+     * 前缀修正
+     * @param $path 路径名称
+     * @param bool $hasSlash 是否带前缀斜杠,默认否
+     * @return false|string
+     * hotarunohikari
+     */
+    static function drFixPrefix($path, $hasSlash = false) {
         $prefix = substr($path, 0, 1);
         if ($prefix == "/" || $prefix == "\\") {
             $path = substr($path, 1);
+            $path = $hasSlash ? '/' . $path : $path;
+        } else {
+            $path = $hasSlash ? '/' . $path : $path;
         }
         return $path;
     }
 
-    // 尾缀修正
-    static function drFixSuffix($path) {
+    /**
+     * 尾缀修正
+     * @param $path 路径名称
+     * @param bool $hasSlash 是否带前缀斜杠,默认否
+     * @return false|string
+     * hotarunohikari
+     */
+    static function drFixSuffix($path, $hasSlash = true) {
         $suffix = substr($path, -1, 1);
-        if (!($suffix == "/" || $suffix == "\\")) {
-            $path = $path . "/";
+        if ($suffix == "/" || $suffix == "\\") {
+            $path = substr($path, 0, -1);
+            $path = $hasSlash ? $path . "/" : $path;
+        } else {
+            $path = $hasSlash ? $path . "/" : $path;
         }
         return $path;
+    }
+
+    /**
+     * 修正文件路径名称
+     * @param $path 路径名称
+     * @param bool $hasSlash 是否带前后缀斜杠,默认是
+     * @return false|string
+     * hotarunohikari
+     */
+    static function drFixDirName($path, $hasSlash = true){
+        return self::drFixSuffix(self::drFixPrefix($path,$hasSlash),$hasSlash);
     }
 
     // 根据日期创建目录,多用于文件上传存储
